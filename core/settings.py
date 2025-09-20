5526c85d62b7058b62e9d5cf4a03fe3d7ee55a48
Lewis-Murimi <lewismurimi195@gmail.com>
2025-09-21 00:17:52 +0300

    updated settings.py

--- core/settings.py
+++ core/settings.py
@@ -78,8 +78,8 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'core.wsgi.application'
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
 
 
 
@@ -184,6 +184,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
