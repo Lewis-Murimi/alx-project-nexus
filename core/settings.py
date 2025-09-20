0e5ea221bddfa5e4f495b8946db1ccbf8b68d539
Lewis-Murimi <lewismurimi195@gmail.com>
2025-09-20 23:40:36 +0300

    updated allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-$guw*545(5$&++(v*m#t_#1vtcqjlo#^4iz%s-$pcx&a2*=@#(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['alx-project-nexus-u4qb.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
