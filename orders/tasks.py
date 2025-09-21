fb8a06f8f7831616b6b08973588a103e115066a5
Lewis-Murimi <lewismurimi195@gmail.com>
2025-09-21 09:36:05 +0300

    updated order tasks.py

--- orders/tasks.py
+++ orders/tasks.py
@@ -8,9 +8,13 @@ from .models import Order
 def send_order_confirmation_email(order_id):
     try:
         order = Order.objects.get(id=order_id)
+        message = (
+            f"Hi {order.user.first_name} {order.user.last_name}, "
+            f"your order totaling ${order.total_price} has been placed successfully!"
+        )
         send_mail(
             subject=f"Order #{order.id} Confirmation",
-            message=f"Hi {order.user.first_name} {order.user.last_name}, your order totaling ${order.total_price} has been placed successfully!",
+            message=message,
             from_email="no-reply@ecommerce.com",
             recipient_list=["lewismurimi195@gmail.com"],
         )
