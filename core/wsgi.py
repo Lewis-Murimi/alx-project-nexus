d5534ea132f3af84c324c48068ec14e2832e51cf
Lewis-Murimi <lewismurimi195@gmail.com>
2025-09-21 16:47:53 +0300

    updated wsgi.py to create superuser on startup

--- core/wsgi.py
+++ core/wsgi.py
@@ -11,6 +11,18 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 application = get_wsgi_application()
+
+# --- Auto create superuser ---
+from django.contrib.auth import get_user_model
+from decouple import config
+
+User = get_user_model()
+username = config("DJANGO_SUPERUSER_USERNAME", default="admin")
+email = config("DJANGO_SUPERUSER_EMAIL", default="admin@ecommerce.com")
+password = config("DJANGO_SUPERUSER_PASSWORD", default="Password@123")
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
