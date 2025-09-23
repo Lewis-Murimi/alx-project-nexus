30a4ef0a3bada03c98bcb5911ad923eaf056d600
Lewis-Murimi <lewismurimi195@gmail.com>
2025-09-23 20:03:21 +0300

    fixed bug on email settings

--- core/settings.py
+++ core/settings.py
@@ -161,7 +161,7 @@ SWAGGER_SETTINGS = {
 CACHE_TTL = 60 * 15  # 15 minutes
 
 # Email configuration
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
